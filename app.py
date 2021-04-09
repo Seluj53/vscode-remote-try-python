4ac3efc87d51fe5604080eeb2aa1c3f56a3ee62c
Jules L <52790189+Seluj53@users.noreply.github.com>
2021-04-09 17:13:11 +0000

    cc

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------
 
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return app.send_static_file("index.html")
+
+
+app.run(host="localhost", port="9000")
